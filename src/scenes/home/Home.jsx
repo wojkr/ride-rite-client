37213170261004fe807608dcf6c30a9ffe6bbb7c
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-10-27 23:40:22 +0100

    added redirection/scroll func

--- src/scenes/home/Home.jsx
+++ src/scenes/home/Home.jsx
@@ -2,8 +2,20 @@ import MainCarousel from "./MainCarousel";
 import ShoppingList from "./ShoppingList";
 import Subscribe from "./Subscribe";
 import About from "./About";
+import { useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
+  const location = useLocation();
+  const sectionId = location.hash.substring(1);
+  useLayoutEffect(() => {
+    console.log(sectionId);
+    // const hash = window.location.hash.substring(3);
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView();
+    }
+  }, [location]);
   return (
     <div className="home pb-3">
       <MainCarousel />
