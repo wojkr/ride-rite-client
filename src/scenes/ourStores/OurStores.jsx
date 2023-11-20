1ef1c1c2d3948660e14b83a23dff716b9f4518f3
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-11-20 14:54:49 +0000

    fixing marker bug

--- src/scenes/ourStores/OurStores.jsx
+++ src/scenes/ourStores/OurStores.jsx
@@ -6,6 +6,16 @@ import { category } from "../../Model/category";
 import { shades } from "../../theme";
 import Store from "./Store";
 
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
 const stores = [
   {
     title: "London",
