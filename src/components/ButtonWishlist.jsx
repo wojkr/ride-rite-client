fbfabde6cbf972dce0bdf79374e34bb98ef35287
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-12-01 23:38:14 +0000

    implemented editable wishlist

--- src/components/ButtonWishlist.jsx
+++ src/components/ButtonWishlist.jsx
@@ -1,45 +1,61 @@
 import { FavoriteBorderOutlined, FavoriteOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
-import React, { useEffect } from "react";
+import React from "react";
 import { useCookies } from "react-cookie";
 import { cookieName } from "../Model/cookies";
-import { shallowEqual, useSelector } from "react-redux";
-
-async function updateWishlist(token, wishlist) {
-  if (!!cookie[cookieName] && cookie[cookieName] != "undefined") {
-    const url = `${serverUrl}/api/users/me?populate=wishlist&populate=billingAddress&populate=shippingAddress&populate=orders`;
-    await axios
-      .get(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        console.log(response.data);
-        dispatch(setLoggedIn(response.data));
-      })
-      .catch((error) => {
-        // Handle error.
-        console.error("An error occurred:", error.response);
-      });
-  } else {
-    console.warn("user not logged in!");
-  }
-}
-
-const handleWishlistRequest = (id) => {
-  console.info("REQ!!" + id);
-};
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { serverUrl } from "../serverUrl";
+import { setWishlist } from "../state/user";
 
 const ButtonWishlist = ({ isAdded = false, backgroundColor = "", id }) => {
   const [cookie] = useCookies([cookieName]);
   const token = cookie[cookieName];
   const user = useSelector((state) => state.user.user, shallowEqual);
-  console.log;
+  const wishlist = useSelector((state) => state.user.user.wishlist);
+  const items = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+  const itemId = parseInt(id);
+  let wishlistIds = wishlist.map((i) => i.id);
+  const handleWishlistRequest = async (itemId) => {
+    if (wishlistIds.includes(itemId)) {
+      //remove item
+      const index = wishlistIds.indexOf(itemId);
+      wishlistIds.splice(index, 1);
+    } else {
+      //add
+      wishlistIds.push(itemId);
+    }
+    updateUserData(user.id, token, wishlistIds);
+  };
+
+  const updateUserData = async (userId, token, wishlistIdArray) => {
+    try {
+      const response = await axios.put(
+        `${serverUrl}/api/users/${userId}`,
+        {
+          updateWishlist: {
+            wishlist: wishlistIdArray,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const item = items.find((item) => item.id == itemId);
+      dispatch(setWishlist(item));
+      return response.data;
+    } catch (error) {
+      console.error("Error updating user data:", error);
+      throw error;
+    }
+  };
 
   return (
     <IconButton
-      onClick={() => handleWishlistRequest(id)}
+      onClick={() => handleWishlistRequest(itemId)}
       sx={{
         color: "black",
         backgroundColor: backgroundColor,
--- src/state/user.js
+++ src/state/user.js
@@ -17,10 +17,20 @@ export const userSlice = createSlice({
       state.user = action.payload;
       state.isLoggedIn = true;
     },
+    setWishlist: (state, action) => {
+      const itemId = action.payload.id;
+      const wishlist = state.user.wishlist;
+      const index = wishlist.findIndex((item) => item.id === itemId);
+      if (index < 0) {
+        wishlist.push(action.payload);
+      } else {
+        state.user.wishlist = wishlist.filter((item) => item.id !== itemId);
+      }
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setLoggedOut, setLoggedIn } = userSlice.actions;
+export const { setLoggedOut, setLoggedIn, setWishlist } = userSlice.actions;
 
 export default userSlice.reducer;
