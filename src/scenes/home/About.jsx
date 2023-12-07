a72120aaa922275f721808bbb43e4721de976853
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-12-07 14:46:51 +0000

    added btn go to our stores

--- src/scenes/home/About.jsx
+++ src/scenes/home/About.jsx
@@ -1,10 +1,12 @@
-import { Box, Divider, Typography, useMediaQuery } from "@mui/material";
+import { Box, Button, Divider, Typography, useMediaQuery } from "@mui/material";
 import { useState } from "react";
-import { shades } from "../../theme";
+import { shades, theme } from "../../theme";
+import { useNavigate } from "react-router-dom";
+import { ourstores } from "../../Model/menu";
 
 const About = () => {
-  const [email, setEmail] = useState("");
   const isNonMobile = useMediaQuery("(min-width:1200px");
+  const navigate = useNavigate();
   return (
     <Box
       id="About"
@@ -21,40 +23,72 @@ const About = () => {
         display="flex"
         flexDirection={isNonMobile ? "row" : "column"}
         justifyContent="space-evenly"
-        alignItems="baseline"
         gap={isNonMobile ? "5rem" : "1rem"}
       >
-        <Box flex="5" paddingLeft={isNonMobile && "10%"} width="80%">
-          <Typography
-            variant="h2"
-            fontWeight="bold"
-            lineHeight="150%"
-            color={shades.neutral[600]}
-          >
-            Welcome to Ride Rite
+        <Box
+          flex="5"
+          paddingLeft={isNonMobile && "10%"}
+          width={isNonMobile ? "80%" : "100%"}
+        >
+          <Typography variant="h3" color={shades.neutral[600]}>
+            Welcome to
           </Typography>
           <Typography
-            variant="h3"
+            variant="h1"
+            fontSize="88px"
+            lineHeight="80%"
             fontWeight="bold"
-            lineHeight="150%"
-            color={shades.neutral[600]}
+            sx={{
+              background: `linear-gradient(8deg, ${
+                shades.neutral[600] + "AA"
+              } 30%, ${shades.neutral[700]} 90%)`,
+              WebkitBackgroundClip: "text",
+              color: "transparent",
+            }}
           >
+            Ride Rite
+          </Typography>
+          <Typography fontSize="20px" color={shades.neutral[600]}>
             {" "}
-            your online destination for high-quality bicycles and accessories.
+            your go-to for top-notch bikes and gear!
           </Typography>
         </Box>
         <Box flex="7">
-          <Typography color={shades.neutral[800]} lineHeight="225%">
-            We offer a wide selection of bikes, including road bikes, mountain
-            bikes, electric bikes, and hybrids, as well as a range of
-            accessories such as helmets, locks, lights, and apparel. Our
-            experienced team is dedicated to providing exceptional customer
-            service and helping you find the perfect bike and gear to suit your
-            needs. We source our products from top manufacturers to ensure the
-            best quality and performance, and offer competitive pricing and fast
-            shipping. Thank you for choosing Ride Rite for all your cycling
-            needs!
+          <Typography
+            lineHeight="205%"
+            fontSize="18px"
+            color={shades.primary[600]}
+          >
+            Explore bikes, gear, and accessories at Ride Rite! Top-quality
+            products, friendly service, and fast shipping. Your go-to for all
+            things cycling!
           </Typography>
+          <Box textAlign="center">
+            <Button
+              variant="contained"
+              aria-label="our stores link"
+              onClick={() => navigate(ourstores.link())}
+              sx={{
+                marginTop: "2rem",
+                mb: "1rem",
+                backgroundColor: shades.neutral[600],
+                color: "white",
+                "&:hover": {
+                  backgroundColor: shades.neutral[400],
+                  color: "black",
+                },
+              }}
+            >
+              Check out our stores!
+            </Button>
+            <Typography
+              fontFamily={theme.typography.h1.fontFamily}
+              color={shades.neutral[600]}
+            >
+              Explore Nearby Stores! 🌍 Navigate our interactive map and
+              discover our stores.
+            </Typography>
+          </Box>
         </Box>
       </Box>
       <Divider sx={{ background: shades.neutral[300] }} />
